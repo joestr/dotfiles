85bad9c8f4fda2709c0795d64bfabfad430e4544
Joel Strasser <strasser999@gmail.com>
2020-03-13 22:13:29 +0100

    added install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+wget https://github.com/joestr/dotfiles/blob/master/.bashrc -O ~/.bashrc
+wget https://github.com/joestr/dotfiles/blob/master/.nanorc -O ~/.nanorc
+wget https://github.com/joestr/dotfiles/blob/master/.profile -O ~/.profile
+exit
