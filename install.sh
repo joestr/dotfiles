8b74761272fe52b1fdf2526afb2d52618e62a447
Joel Strasser <strasser999@gmail.com>
2020-03-25 00:21:14 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+apt install bash-completion
 wget https://raw.githubusercontent.com/joestr/dotfiles/master/.bashrc -O ~/.bashrc
 wget https://raw.githubusercontent.com/joestr/dotfiles/master/.nanorc -O ~/.nanorc
 wget https://raw.githubusercontent.com/joestr/dotfiles/master/.profile -O ~/.profile
