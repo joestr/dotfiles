aaf7f11d16f54aac01f1643c6db0c9c5c0dd2488
Joel Strasser <strasser999@gmail.com>
2020-05-24 16:49:49 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,4 +4,5 @@ wget https://raw.githubusercontent.com/joestr/dotfiles/master/.bashrc -O ~/.bash
 wget https://raw.githubusercontent.com/joestr/dotfiles/master/.nanorc -O ~/.nanorc
 wget https://raw.githubusercontent.com/joestr/dotfiles/master/.bash_login -O ~/.bash_login
 wget https://raw.githubusercontent.com/joestr/dotfiles/master/.bash_aliases -O ~/.bash_aliases
+wget https://raw.githubusercontent.com/joestr/dotfiles/master/.bash_logout -O ~/.bash_logout
 exit
