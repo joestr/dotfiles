35cc4cc73090426548ee859897adf9072bbacd23
Joel Strasser <joelstrasser1@gmail.com>
2023-01-17 12:34:32 +0100

    removes space in front of ">"

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,5 @@
+$env:LC_ALL="C.UTF-8"
+$env:LESSCHARSET="UTF-8"
+
 $host.ui.rawui.windowtitle="$env:username@$($env:computername):$PWD"
-function Global:prompt {"$([char]27)[0m$([char]27)[37m[PS] $([char]27)[95m$env:username$([char]27)[37m@$([char]27)[92m$env:computername$([char]27)[37m:$([char]27)[94m$PWD$(if (Test-Path -Path $PWD/.git) { " $([char]27)[0m$([char]27)[33m[Git branch: $(git branch --show-current)]" })$([char]27)[0m $([char]27)[37m> "}
+function Global:prompt {"$([char]27)[0m$([char]27)[37m[PS] $([char]27)[95m$env:username$([char]27)[37m@$([char]27)[92m$env:computername$([char]27)[37m:$([char]27)[94m$PWD$(if (Test-Path -Path $PWD/.git) { " $([char]27)[0m$([char]27)[33m[Git branch: $(git branch --show-current)]" })$([char]27)[0m$([char]27)[37m> "}
