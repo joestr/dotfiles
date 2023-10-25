e55f91b2314b2f0ed6fab30ac16e1e6741d4c60c
Joel Strasser <strasser999@gmail.com>
2023-10-25 16:51:15 +0200

    include .vimrc

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 apt install bash-completion
 wget https://raw.githubusercontent.com/joestr/dotfiles/master/.bashrc -O ~/.bashrc
 wget https://raw.githubusercontent.com/joestr/dotfiles/master/.nanorc -O ~/.nanorc
+wget https://raw.githubusercontent.com/joestr/dotfiles/master/.vimrc -O ~/.vimrc
 wget https://raw.githubusercontent.com/joestr/dotfiles/master/.bash_login -O ~/.bash_login
 wget https://raw.githubusercontent.com/joestr/dotfiles/master/.bash_aliases -O ~/.bash_aliases
 wget https://raw.githubusercontent.com/joestr/dotfiles/master/.bash_logout -O ~/.bash_logout
