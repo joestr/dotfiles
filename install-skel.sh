8b57342bb1286de6d175c95b158e9ee519381425
joestr <joelstrasser1@gmail.com>
2024-11-22 17:08:36 +0100

    feat: remove auto install scripts because user should not run untested bash scripts

--- install-skel.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-apt install bash-completion
-wget https://raw.githubusercontent.com/joestr/dotfiles/master/.bashrc -O /etc/skel/.bashrc
-wget https://raw.githubusercontent.com/joestr/dotfiles/master/.nanorc -O /etc/skel/.nanorc
-wget https://raw.githubusercontent.com/joestr/dotfiles/master/.vimrc -O /etc/skel/.vimrc
-wget https://raw.githubusercontent.com/joestr/dotfiles/master/.bash_login -O /etc/skel/.bash_login
-wget https://raw.githubusercontent.com/joestr/dotfiles/master/.bash_aliases -O /etc/skel/.bash_aliases
-wget https://raw.githubusercontent.com/joestr/dotfiles/master/.bash_logout -O /etc/skel/.bash_logout
-exit
--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-apt install bash-completion
-wget https://raw.githubusercontent.com/joestr/dotfiles/master/.bashrc -O ~/.bashrc
-wget https://raw.githubusercontent.com/joestr/dotfiles/master/.nanorc -O ~/.nanorc
-wget https://raw.githubusercontent.com/joestr/dotfiles/master/.vimrc -O ~/.vimrc
-wget https://raw.githubusercontent.com/joestr/dotfiles/master/.bash_login -O ~/.bash_login
-wget https://raw.githubusercontent.com/joestr/dotfiles/master/.bash_aliases -O ~/.bash_aliases
-wget https://raw.githubusercontent.com/joestr/dotfiles/master/.bash_logout -O ~/.bash_logout
-exit
