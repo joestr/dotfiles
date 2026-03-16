a7114f37c8040664ef206d5a8d2c79981f19f1b4
Joel Strasser <joelstrasser1@gmail.com>
2026-03-16 16:46:38 +0100

    feat: add Vcs state from posh-git

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -2,4 +2,16 @@ $env:LC_ALL="C.UTF-8"
 $env:LESSCHARSET="UTF-8"
 
 $host.ui.rawui.windowtitle="$env:username@$($env:computername):$PWD"
-function Global:prompt {"$([char]27)[0m$([char]27)[37m[PS] $([char]27)[95m$env:username$([char]27)[37m@$([char]27)[92m$env:computername$([char]27)[37m:$([char]27)[94m$PWD$(if (Test-Path -Path $PWD/.git) { " $([char]27)[0m$([char]27)[33m[Git branch: $(git branch --show-current)]" })$([char]27)[0m$([char]27)[37m> "}
+function Global:prompt {
+    $result = "$([char]27)[0m$([char]27)[37m[PS]"
+    $result += " $([char]27)[95m$env:username$([char]27)[37m"
+    $result += "@"
+    $result += "$([char]27)[92m$env:computername"
+    $result += "$([char]27)[37m:"
+    $result +=  "$([char]27)[94m$PWD"
+    $result += "$(Write-VcsStatus)"
+    $result += "$([char]27)[0m$([char]27)[37m$('>' * ($nestedPromptLevel + 1))"
+    return $result
+}
+
+Import-Module posh-git
